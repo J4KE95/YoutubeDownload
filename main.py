15086f7efd44076e674457bfd7cde5b341774077
J4KE95 <47159800+J4KE95@users.noreply.github.com>
2020-10-30 18:27:53 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import pytube
+import time
+
+def downloader () :
+    print("Please Wait For Video To Download...")
+    get_video = pytube.YouTube(url)
+    streams = get_video.streams.first()
+    streams.download()
+    print("Download Complete!")
+    print("Downloads Can Be Found In The YoutubeDownload Folder.")
+
+def converter () :
+    print("Please Wait For Video To Download...")
+    get_video = pytube.YouTube(url)
+    streams = get_video.streams.filter(only_audio=True).first()
+    streams.download()
+    print("Download Complete!")
+    print("Downloads Can Be Found In The Folder Where The main.py File Is Stored.")
+
+print("Welcome To YoutubeDownload!")
+time.sleep(1)
+url = input("Please Paste Link To Youtube Video: ")
+
+def select () :
+    print(f"You Have Selected: {pytube.YouTube(url).title}")
+    print("Press 1 To Download Youtube Video")
+    print("Press 2 To Convert Youtube Video To Audio Format And Download")
+    selection = input("Please Choose An Option: ")
+    if selection == "1" :
+        downloader()
+    elif selection == "2" :
+        converter()
+    else :
+        print("Invalid Selection!")
+        time.sleep(1)
+        select()
+select()
